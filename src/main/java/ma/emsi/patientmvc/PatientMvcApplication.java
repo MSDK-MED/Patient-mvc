7a72d0e9388e52bba036310de960dcd3eb9b257d
msdk00 <mohamed.moussaddik00@gmail.com>
2023-04-27 00:00:23 +0100

    Initial commit

---
+++ src/main/java/ma/emsi/patientmvc/PatientMvcApplication.java
@@ -0,0 +1,26 @@
+package ma.emsi.patientmvc;
+
+import ma.emsi.patientmvc.entities.Patient;
+import ma.emsi.patientmvc.repositories.PatientRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class PatientMvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PatientMvcApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args ->{
+            patientRepository.save(new Patient(null,"hassan",new Date(),true,23));
+            patientRepository.save(new Patient(null,"amine",new Date(),false,333));
+            patientRepository.save(new Patient(null,"moha",new Date(),false,253));
+
+        };
+    }
+}
---
+++ src/main/java/ma/emsi/patientmvc/entities/Patient.java
@@ -0,0 +1,23 @@
+package ma.emsi.patientmvc.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Patient {
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    @Temporal(TemporalType.DATE)
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
